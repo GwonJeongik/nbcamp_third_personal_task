090429f98ab34d320691f943501bc9411a0d7b17
ik <jeongik11029@naver.com>
2024-08-28 12:21:57 +0900

    Initial commit

---
+++ src/main/java/nbcamp/thirdpersonaltask/ThirdPersonalTaskApplication.java
@@ -0,0 +1,13 @@
+package nbcamp.thirdpersonaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPersonalTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdPersonalTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nbcamp/thirdpersonaltask/ThirdPersonalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package nbcamp.thirdpersonaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdPersonalTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
